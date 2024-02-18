8e3e4d731282537c2b99ee553fb2b267e34db289
JaeSeok <dhwotjr7755@gmail.com>
2024-02-18 15:48:40 -0600

    PS4

---
+++ ProblemSets/PS4/PS4a_Oh.R
@@ -0,0 +1,16 @@
+url <- "https://www.vizgr.org/historical-events/search.php?format=json&begin_date=00000101&end_date=20240209&lang=en"
+destination_file <- "downloaded_file.json"
+download.file(url, destfile = destination_file, method = "auto")
+
+
+wget -O dates.json "https://www.vizgr.org/historical-events/search.php?format=json&begin_date=00000101&end_date=20240209&lang=en"
+
+library(tidyverse)
+library(jsonlite)
+
+setwd("/Users/jaeseokoh/Oklahoma_University/Spring2024/DScourseS24/ProblemSets/PS4")
+
+mylist <- fromJSON('dates.json')
+mydf <- bind_rows(mylist$result[-1])
+class(mydf$date)
+head(mydf, 10)
